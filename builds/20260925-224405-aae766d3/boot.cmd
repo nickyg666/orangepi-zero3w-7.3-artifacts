ea64a71ca0902241890eae829aaaea9df0c0ab95
PA <pa@opikernel.local>
2026-09-25 18:44:21 -0400

    7.3 build 20260925-224405-aae766d3: Image aae766d315fe (41707528 B) VA_BITS=39 EFI_STUB=(absent)

---
+++ builds/20260925-224405-aae766d3/boot.cmd
@@ -0,0 +1,7 @@
+setenv bootargs console=ttyS0,115200 earlycon=uart,mmio32,0x2500000 root=PARTUUID=486ea4ae-01 rootfstype=ext4 rootwait rw modprobe.blacklist=aic8800_btlpm loglevel=7
+ext4load mmc 0:1 0x41000000 boot/mainline/Image
+cp.b 0x41000000 0x40200000 0x27BC808
+ext4load mmc 0:1 0x43000000 boot/mainline/sun60i-a733-orangepi-zero3w.dtb
+fdt addr 0x43000000
+fdt resize 65536
+booti 0x40200000 - 0x43000000
